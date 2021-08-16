9070485f17364d06388ffa2519235f34563ebc3f
parshuram10 <88996528+parshuram10@users.noreply.github.com>
2021-08-16 19:03:38 +0530

    Initial Commit

---
+++ Project/admin.js
@@ -0,0 +1,19 @@
+$('.add-btn').click(function(){
+	window.location.href='add.html';
+});
+
+$('.update-btn').click(function(){
+	window.location.href='update.html';
+});
+
+$('.del-btn').click(function(){
+	window.location.href='admin.html';
+});
+
+$('.search-btn').click(function(){
+	window.location.href='admin.html';
+});
+
+$('.order-btn').click(function(){
+	window.location.href='admin.html';
+});
---
+++ Project/library.js
@@ -0,0 +1,6 @@
+$('.admin-btn').click(function(){
+	window.location.href='admin.html';
+});
+$('.student-btn').click(function(){
+	window.location.href='student.html';
+});
